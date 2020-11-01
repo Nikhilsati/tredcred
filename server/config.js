efea06a81529dd412bf108f896982a4113084631
Nikhilsati <nikhil121sati.5@gmail.com>
2020-11-01 21:52:49 +0530

    intial commit

---
+++ server/config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBk8txVx_dubOnzFl3suUVHYcaPtTq7jAQ",
+    authDomain: "tradecred-88e65.firebaseapp.com",
+    databaseURL: "https://tradecred-88e65.firebaseio.com",
+    projectId: "tradecred-88e65",
+    storageBucket: "tradecred-88e65.appspot.com",
+    messagingSenderId: "3031335212",
+    appId: "1:3031335212:web:ed69bb52284178749eac89"
+  };
+
+module.exports = firebaseConfig;
